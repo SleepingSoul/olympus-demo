01717d8eb7dd3365f1fb3ca12285a1835ebb8516
Tihran Katolikian <moreggmoreskill@icloud.com>
2020-01-02 16:28:45 +0200

    Add types

--- olympus/code/engine/types.h
+++ olympus/code/engine/types.h
@@ -4,8 +4,25 @@
 
 namespace oly
 {
+    // containers
     using Vector = std::vector;
     using Array = std::array;
     using String = std::string;
     using StringView = std::string_view;
+
+    // integral types
+    using int32 = std::int32_t;
+    using uint32 = std::uint32_t;
+    using byte = std::uint8_t;
+    using int8 = std::int8_t;
+    using uint8 = std::uint8_t;
+    using int64 = std::int64_t;
+    using uint64 = std::uint64_t;
+
+    // floating point types
+    using float32 = float;
+    static_assert(sizeof(float) == 4);
+
+    using float64 = double;
+    static_assert(sizeof(double) == 8);
 }
--- olympus/code/stdafx.h
+++ olympus/code/stdafx.h
@@ -2,6 +2,7 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <cstdint>
 #include <Windows.h>
 
 #undef min
