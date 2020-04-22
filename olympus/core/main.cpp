0b6d9b5b2b0b2df10203485e09e7cfb7121cc77f
Tihran Katolikian <moreggmoreskill@icloud.com>
2020-04-22 22:21:37 +0300

    Clean up external commons

--- olympus/core/main.cpp
+++ olympus/core/main.cpp
@@ -9,10 +9,27 @@ int main(int argc, char** argv)
 {
     profiler::startListen();
 
-    EASY_FUNCTION(profiler::colors::Magenta);
+    cxxopts::Options options("olympus", "Augumented reality engine");
+
+    options.add_options()
+        ("w,width", "window width in px", cxxopts::value<unsigned>()->default_value("800"))
+        ("h,height", "window height in px", cxxopts::value<unsigned>()->default_value("600"));
 
     auto logger = spdlog::basic_logger_mt("logger works!", "logger works.txt");
     logger->debug(estd::format("ESTD works, {}!", "yay"));
+
+    try
+    {
+        auto result = options.parse(argc, argv);
+        auto value = result["width"].as<unsigned>();
+    }
+    catch (const cxxopts::OptionException& e)
+    {
+        logger->error(e.what());
+    }
+
+    EASY_FUNCTION(profiler::colors::Magenta);
+
 
     return 0;
 }
