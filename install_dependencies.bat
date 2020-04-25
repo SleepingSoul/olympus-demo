3f49d6d62fae55881f72e4982eb25dfb93d782a2
Tihran Katolikian <moreggmoreskill@icloud.com>
2020-04-25 18:49:25 +0300

    Add install_dependencies script

---
+++ install_dependencies.bat
@@ -0,0 +1,14 @@
+@echo Project dependencies will be installed with vcpkg.
+
+cd vcpkg
+
+@echo Installing vcpkg...
+
+call bootstrap-vcpkg.bat
+
+@echo Installing dependencies...
+
+vcpkg.exe install docopt:x64-windows glad:x64-windows glfw3:x64-windows glm:x64-windows imgui:x64-windows ^
+nlohmann-json:x64-windows opengl:x64-windows spdlog:x64-windows stb:x64-windows
+
+@echo Done.
