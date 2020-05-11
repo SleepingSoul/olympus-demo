47bcdb14d216fe7b3a3f0973895e8d7976e819c2
Tihran Katolikian <moreggmoreskill@icloud.com>
2020-05-11 14:33:42 +0300

    Implement texture class

--- olympus/renderer/Texture.cpp
+++ olympus/renderer/Texture.cpp
@@ -1,66 +1,81 @@
-//#include "Texture.h"
-//
-//#pragma warning(push, 1)
-//#define STB_IMAGE_IMPLEMENTATION
-//#include <stb_image.h>
-//#pragma warning(pop)
-//
-//#include "utils.h"
-//
-//
-//namespace
-//{
-//    std::array<std::filesystem::path, 6> faces{
-//        "right.jpg",
-//        "left.jpg",
-//        "top.jpg",
-//        "bottom.jpg",
-//        "front.jpg",
-//        "back.jpg"
-//    };
-//
-//    GLenum channelsNumberToFormat(int channelsNumber)
-//    {
-//        switch (channelsNumber)
-//        {
-//        case 1:
-//            return GL_RED;
-//        case 3:
-//            return GL_RGB;
-//        case 4:
-//            return GL_RGBA;
-//        default:
-//            return GL_NONE;
-//        }
-//    }
-//}
-//
-//oly::CubeMap::CubeMap(const std::filesystem::path& cubeMapTextures)
-//{
-//    glCreateTextures(GL_TEXTURE_CUBE_MAP, 1, &m_id);
-//
-//    for (size_t i = 0; i < 6; ++i)
-//    {
-//        auto& currentTextureData = m_cubeMapTextures.at(i);
-//
-//        auto path = faces[i];
-//        stbi_uc* const data = stbi_load(path.u8string().c_str(), &currentTextureData.width, &currentTextureData.height,
-//            &currentTextureData.numChannels, 0);
-//
-//        if (assertcheck(!data, "Failed to load texture!"))
-//        {
-//            continue;
-//        }
-//
-//        const auto format = channelsNumberToFormat(currentTextureData.numChannels);
-//
-//        glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + static_cast<GLenum>(i), 0, format, currentTextureData.width, currentTextureData.height, 0,
-//            format, GL_UNSIGNED_BYTE, data);
-//
-//        stbi_image_free(data);
-//    }, GL_CLAMP_TO_EDGE);
-//    glTextureParameteri(m_id, GL_TEXTURE_WRAP_S,
-//    glTextureParameteri(m_id, GL_TEXTURE_WRAP_T GL_CLAMP_TO_EDGE);
-//    glTextureParameteri(m_id, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-//    glTextureParameteri(m_id, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-//}
+#include "Texture.h"
+
+#include <stb_image.h>
+
+#include <utils/olyerror.h>
+
+#include <utils.h>
+
+BeginNamespaceOlympus
+
+namespace
+{
+    GLenum channelsNumberToFormat(int channelsNumber)
+    {
+        switch (channelsNumber)
+        {
+        case 1:
+            return GL_RED;
+        case 3:
+            return GL_RGB;
+        case 4:
+            return GL_RGBA;
+        default:
+            return GL_NONE;
+        }
+    }
+}
+
+Texture::Texture(const std::filesystem::path& textureFile)
+{
+    glCreateTextures(GL_TEXTURE_2D, 1, &m_id);
+
+    glBindTexture(GL_TEXTURE_2D, m_id);
+
+    stbi_uc* const data = stbi_load(textureFile.u8string().c_str(), &m_width, &m_height, &m_numChannels, 0);
+
+    if (!data)
+    {
+        olyError("[Texture] texture load failed (path is: '{}')", textureFile);
+        return;
+    }
+
+    const auto format = channelsNumberToFormat(m_numChannels);
+
+    if (format == GL_NONE)
+    {
+        olyError("[Texture] unrecognized format of the given image: '{}'", textureFile);
+        return;
+    }
+
+    glTexImage2D(GL_TEXTURE_2D, 0, format, m_width, m_height, 0, format, GL_UNSIGNED_BYTE, data);
+
+    stbi_image_free(data);
+
+    glTextureParameteri(m_id, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTextureParameteri(m_id, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTextureParameteri(m_id, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTextureParameteri(m_id, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    m_loadSuccess = true;
+}
+
+Texture::~Texture()
+{
+    glDeleteTextures(1, &m_id);
+}
+
+void Texture::hotReset(const void* data, GLsizei width, GLsizei height, GLenum format)
+{
+    if (width != m_width || height != m_height)
+    {
+        olyError("[Texture] texture hot reset with different size values is unsupported.");
+        return;
+    }
+
+    glTextureSubImage2D(m_id, 0, 0, 0, width, height, format, GL_UNSIGNED_BYTE, data);
+}
+
+EndNamespaceOlympus
--- olympus/renderer/Texture.h
+++ olympus/renderer/Texture.h
@@ -1,33 +1,37 @@
 #pragma once
 
 #include <filesystem>
-#include <array>
 #include <glad/glad.h>
-#include <glm/glm.hpp>
 
 #include <utils/macros.h>
 
 BeginNamespaceOlympus
 
-class CubeMap
+class Texture
 {
+    OlyNonCopyable(Texture)
 public:
     using TextureID = GLuint;
 
-    struct TextureData
-    {
-        GLsizei width{ 0 };
-        GLsizei height{ 0 };
-        int numChannels{ 0 };
-    };
+    // Will load the texture from the given file
+    Texture(const std::filesystem::path& textureFile);
+    ~Texture();
 
-    CubeMap(const std::filesystem::path& cubeMapTextures);
+    TextureID getID() const { return m_id; }
+    GLsizei getWidth() const { return m_width; }
+    GLsizei getHeight() const { return m_height; }
 
-    auto getID() const { return m_id; }
+    bool isValid() const { return m_id != 0 && m_loadSuccess; }
+
+    void hotReset(const void* data, GLsizei width, GLsizei height, GLenum format);
 
 private:
     TextureID m_id{ 0 };
-    std::array<TextureData, 6> m_cubeMapTextures;
+    bool m_loadSuccess{ false };
+
+    GLsizei m_width{ 0 };
+    GLsizei m_height{ 0 };
+    int m_numChannels{ 0 };
 
     std::filesystem::path m_path;
 };
