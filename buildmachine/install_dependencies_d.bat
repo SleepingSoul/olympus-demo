6ffa211e1dabf2b16beefdf9a7503b09129b7f23
Tihran Katolikian <moreggmoreskill@icloud.com>
2020-06-21 12:48:54 +0300

    Build only debug libs for begun buildmachine configuration

---
+++ buildmachine/install_dependencies_d.bat
@@ -0,0 +1,17 @@
+@echo Project dependencies will be installed, but only for release configuration. Use this scipt only on buldmachine.
+
+@echo Installing vcpkg...
+
+call %~dp0..\vcpkg\bootstrap-vcpkg.bat
+
+@echo Fixing triplets...
+
+copy /y %~dp0x64-windows-donly.cmake %~dp0..\vcpkg\triplets\x64-windows.cmake
+
+@echo Installing debug only libs...
+
+%~dp0..\vcpkg\vcpkg.exe install docopt:x64-windows glad:x64-windows glfw3:x64-windows glm:x64-windows ^
+nlohmann-json:x64-windows opengl:x64-windows spdlog:x64-windows stb:x64-windows boost-circular-buffer:x64-windows ^
+curl:x64-windows opencv:x64-windows
+
+@echo Done.
