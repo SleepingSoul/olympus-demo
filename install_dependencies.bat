566892642844b8c5ad703e31c5c92b40785af401
SleepingSoul <moreggmoreskill@icloud.com>
2021-11-22 01:28:37 +0200

    Add assimp to dependencies

--- install_dependencies.bat
+++ install_dependencies.bat
@@ -8,6 +8,6 @@ call %~dp0vcpkg\bootstrap-vcpkg.bat
 
 %~dp0vcpkg\vcpkg.exe install docopt:x64-windows glad:x64-windows glfw3:x64-windows glm:x64-windows ^
 nlohmann-json:x64-windows opengl:x64-windows spdlog:x64-windows stb:x64-windows boost-circular-buffer:x64-windows ^
-curl:x64-windows opencv:x64-windows
+curl:x64-windows opencv:x64-windows assimp:x64-windows
 
 @echo Done.
